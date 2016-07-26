161dd7672f7dc74b9b4b1e4b76a516bf2db556df
Pierre-Adrien MAISON <pierre-adrien.maison@hotmail.fr>
2016-07-26 00:06:16 -0700

    Merge

---
+++ app/src/main/java/com/thelatest/thelatestmobile/SearchActivity.java
@@ -0,0 +1,20 @@
+package com.thelatest.thelatestmobile;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+
+    public void openMainActivity(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
